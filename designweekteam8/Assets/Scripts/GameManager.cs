cdf3387e1a84f2f592a4b7a4ebfca21e8af384df
wightkid <103604852+wightkid@users.noreply.github.com>
2025-02-10 13:45:38 -0500

    Add grid and cell size variables

--- designweekteam8/Assets/Scripts/GameManager.cs
+++ designweekteam8/Assets/Scripts/GameManager.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     int numberOfPlayers = 0;
-
+    public Vector2 gridSize = Vector2.zero;
+    public float cellSize = 1f;
 
     void Start()
     {
+        // Get number of players selected
         numberOfPlayers = PlayerPrefs.GetInt("NumberOfPlayers");
-        Debug.Log(numberOfPlayers);
     }
 
     void Update()
