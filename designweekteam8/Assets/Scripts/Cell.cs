cfa527a583b2687f778e33610e9bfedfd89f5798
brandon <brandon@DESKTOP-KUGH9K4>
2025-02-11 04:01:18 -0500

    Add cell class Add camera centering Add temporary runtime update for cell array

---
+++ designweekteam8/Assets/Scripts/Cell.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public Vector2 position;
+    public Vector2 size;
+    public int renderOrder = 1;
+
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- designweekteam8/Assets/Scripts/GameManager.cs
+++ designweekteam8/Assets/Scripts/GameManager.cs
@@ -11,23 +11,61 @@ public class GameManager : MonoBehaviour
     public Vector2 gridSize = Vector2.zero;
     public float cellSize = 1f;
     int numberOfCells;
+    GameObject cellParent;
+    public GameObject cellPrefab;
+    public Camera mainCam;
+    
 
     void Start()
     {
         // Get number of players selected
         numberOfPlayers = PlayerPrefs.GetInt("NumberOfPlayers");
         numberOfCells = (int)gridSize.x * (int)gridSize.y;
-
         cells = new GameObject[numberOfCells];
+        cellParent = new GameObject("Cells");
 
+        // Create new gameobject, format name, set parent
         for (int i = 0; i < cells.Length; i++)
         {
-            cells[i] = new GameObject($"Cell_{i + 1}");
+            cells[i] = Instantiate(cellPrefab, cellParent.transform);
         }
+        
+        // Instantiate all cells with cell prefabs, and update position
+        // Position is determined by cell size and count
+        int cellIndex = 0;
+        for (int row = 0; row < gridSize.y; row++)
+        { 
+            for (int col = 0; col < gridSize.x; col++)
+            {
+                Vector2 newPosition = new Vector2(col * cellSize, row * cellSize);
+                cells[cellIndex].transform.position = newPosition;
+                cellIndex++;
+            }
+        }
+        UpdateCameraPosition();
     }
      
     void Update()
     {
-        
+        int cellIndex = 0;
+        for (int row = 0; row < gridSize.y; row++)
+        {
+            for (int col = 0; col < gridSize.x; col++)
+            {
+                Vector2 newPosition = new Vector2(col * cellSize, row * cellSize);
+                cells[cellIndex].transform.position = newPosition;
+                cellIndex++;
+            }
+        }
+        UpdateCameraPosition();
+    }
+
+    // Set camera position to the midpoint of the grid matrix
+    void UpdateCameraPosition()
+    {
+        // Update camera
+        float camX = cells[0].transform.position.x + cells[cells.Length - 1].transform.position.x * 0.5f;
+        float camY = cells[0].transform.position.y + cells[cells.Length - 1].transform.position.y * 0.5f;
+        mainCam.transform.position = new Vector3(camX, camY, -0.35f);
     }
 }
