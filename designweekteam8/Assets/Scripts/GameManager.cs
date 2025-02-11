c8e61ba3e4ce5362507617e881b34e898dafacc2
wightkid <103604852+wightkid@users.noreply.github.com>
2025-02-11 03:01:54 -0500

    Add basic grid matrix

--- designweekteam8/Assets/Scripts/GameManager.cs
+++ designweekteam8/Assets/Scripts/GameManager.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     int numberOfPlayers = 0;
+
+    // TODO: change cells from gameObject to cells
+    GameObject[] cells;
     public Vector2 gridSize = Vector2.zero;
     public float cellSize = 1f;
+    int numberOfCells;
 
     void Start()
     {
         // Get number of players selected
         numberOfPlayers = PlayerPrefs.GetInt("NumberOfPlayers");
+        numberOfCells = (int)gridSize.x * (int)gridSize.y;
+
+        cells = new GameObject[numberOfCells];
+
+        for (int i = 0; i < cells.Length; i++)
+        {
+            cells[i] = new GameObject($"Cell_{i + 1}");
+        }
     }
 
     void Update()
